05b9ea12528ebcc4e156c6b70d4c5a6112934f9e
meganliska <ml1623@georgetown.edu>
2016-09-15 11:29:07 -0400

    Fixed in line comments. still need to finish 12

--- Liska_Megan_HW1.py
+++ Liska_Megan_HW1.py
@@ -17,7 +17,7 @@ def translate(words):
     For example translate('merry') will return 'god'
     
     Parameters:
-    words- An English word from our dictionary
+    words- An English word, or words from our dictionary
     
     Returns: 
     The corresponding Swedish word from our dictionary
@@ -28,20 +28,20 @@ def translate(words):
     
     """
     dict2 = {"merry": "god", "christmas": "jul", "and": "och", "happy": "gott",
-             "new": "nytt", "year": "ar"} #We define our dictionary using the words given in the problem.   
+             "new": "nytt", "year": "ar"}  #define our dictionary using the 
+                                           #words given in the problem.   
     result = '' #sets our result as an empty string the function will fill.
 
     for char in words: #loops through the words we input
         swedish = dict2.get(char, char) #searches through dictionary and
 
-#the dictionary dict2, and will return the char that matches something
+
 
         result += swedish #sets results as blank plus swedish 
     
         
-    return result 
-#This displays the result so we can confirm that our function worked correctly.
-print(translate('merry christmas and happy new year'))        
+    return result
+print(translate('merry christmas and happy new year')) #test case       
         
 #Problem 2: Write a function char_freq that takes a string and builds a 
 #frequency list of the characters contained in it.
@@ -60,9 +60,11 @@ def char_freq(text):
     
     ml2016
     """
+    #Can once again use the .get function
     result = {} #sets our result as an dictionary that the function will fill.
     
-    for char in text: #loop will iterate through each character in our text
+    for char in text: 
+    #loop will iterate through each character in our text
         result[char] = result.get(char, 0) + 1 #sets result as  
         
     return result
@@ -83,6 +85,8 @@ def decoder(text):
     
     ml2016
     """
+    #Question is similiar to problem 1
+    #solve in a similiar manner using the .get function
   
     key = {'a':'n', 'b':'o', 
                'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 
@@ -92,23 +96,24 @@ def decoder(text):
                'E':'R', 'F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 
                'L':'Y', 'M':'Z', 'N':'A', 'O':'B', 'P':'C', 'Q':'D', 'R':'E', 
                'S':'F', 'T':'G', 'U':'H', 'V':'I', 'W':'J', 'X':'K', 'Y':'L', 
-               'Z': 'M'}
+               'Z': 'M'} #inputs our dictionary 
 
-    result = ''  
+    result = ''  #sets result as empty string the function will fill
 
     for char in text:  #iterate through the text we input
-        new = key.get(char, char)
-        result += new
+        new = key.get(char, char) #sets new variable as the 
+        result += new #sets our result as the new character
         
     return result
-print(decoder('Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!'))
+print(decoder('Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!'))#test case 
 
 
 #Problem 4: Define a spelling correction function that takes a string and
 #1 Compresses two of more occurences of the space character into one
 #2 Inserts an extra space after a period if the period is directly followed by 
 #a letter.
-#As stated in the tip, we will complete this problem using regular expressions, namely the function
+#As stated in the tip, we will complete this problem using regular expressions,
+#namely the function re.sub
 
 
 import re #imports our regular expression functions including re.sub
@@ -118,18 +123,21 @@ def correct(InputString):
     1:Compresses two or more occurences of the space character
     into one.
     2: Inserts an extra space after a period if the period is directly 
-    folloed by a letter.
+    followed by a letter.
     
     Parameters:
-    InputString- a string of characters or words
+    InputString - a string of characters or words
     
     Results:
     The corrected string of characters or words
     ml2016
     """
-    NewString= re.sub('\s+',' ',InputString) #substitute any instance of one or more spaces with just one space
+    NewString= re.sub('\s+',' ',InputString) #substitute any instance of one 
+                                             #or more spaces with just one space
     
-    NewString= re.sub('\.','. ',NewString) #substitute a period in Input string with a period and a space after it, effectiely adding a space after a period
+    NewString= re.sub('\.','. ',NewString) #substitute a period in Input string 
+                                          #with a period and a space after it,
+                                    #effectiely adding a space after the period
     
     return NewString #returns our result 
 
@@ -153,18 +161,22 @@ def make_3sg_form(words):
    An English verb in third person singular form
    ml2016
    """
+   #use the .endswith('x) function which returns true if our input string ends
+   #with x and false otherwise
+   #use the .endswith function in conjunction with a series of if/else 
+   #statements to test our grammar rules
+   #remember to make the/if else statements from most speicific to most general
 
 
     if words.endswith('y'):
-        return words[0:-1] + 'ies'
-#we are using the .endswith function which returns blank if our input string 
-   #ends with y and 
-  #blank if it doesn't
-#If our input word does end in y our return will be our original word without the last character
-#and with ies added on, thus replacing the y with ies
+        return words[0:-1] + 'ies' #case one, if word ends in y return word
+                                   #without the y and with ies added on
+
+
         
     elif words.endswith('o'):
-        return words + 'es' #case two, if the word ends in o, return the original word with es added to the end        
+        return words + 'es' #case two, if the word ends in o, 
+                            #return the original word with es added to the end        
     
     elif words.endswith('ch'):
         return words + 'es' #This is the same as the case above.   
@@ -177,13 +189,19 @@ def make_3sg_form(words):
     elif words.endswith('z'):
         return words + 'es' #This is the same as the case above.    
     else: 
-        return words + 's' #final case, if the word ends in any other letter than the ones specified
-#our function will return the original words with s added on to the end. 
+        return words + 's' #final case, if the word ends in any other letter 
+                           #than the ones specified
+                           #our function will return the original word 
+                           #with s added on to the end. 
+print(make_3sg_form('try'))
+print(make_3sg_form('brush'))
+print(make_3sg_form('run'))
+print(make_3sg_form('fix'))
         
 
 #Problem 6:
-Con = 'bcdfghjklmnpqrstvwxyz'
-Vowel = 'aeiou'
+Con = 'bcdfghjklmnpqrstvwxyz' #lists all consanants as one variable to simplify
+Vowel = 'aeiou' #lists all vowels as one variable to simplify case 3
 
 def make_ing_form(word):
     """
@@ -196,24 +214,40 @@ def make_ing_form(word):
      Results:
      An English word in present participle form
    
-     
+     ml2016
      """
-    if word in ['be', 'see', 'flee', 'knee']:
+     #Use series of if/else statments to implement different grammar rules
+     #remember to go from most specific case to most general case
+    if word in ['be', 'see', 'flee', 'knee']: #our case of exceptions given in 
+                                             #the original problem
+                                            #If a word is one of these four
+                                                #return orignal word with ing
+                                            #added to the end
         return word + 'ing'
         
-    elif word.endswith('ie'):
+    elif word.endswith('ie'): #if word ends in ie return word with ie taken off
+                              #and ying added to the end of the word
         return word[0:-2] + 'ying'    
     
-    elif word.endswith('e'):
+    elif word.endswith('e'): #if word ends in e return word with e removed and
+                             #ing added to the end, note this have to come after
+                            #the case of excpections since those end in e
         return word[0:-1] + 'ing'
         
    
-    elif word[-3] in Con and word[-2] in Vowel and word[-1] in Con:
-       return word + word[-1] + 'ing'
+    elif word[-3] in Con and word[-2] in Vowel and word[-1] in Con: 
+                                        
+       return word + word[-1] + 'ing' #if word ends in consanent vowel con
+                                       #return word with last letter 
+                                      #doubled and ing added
        
     else:
-        return word + 'ing'
-    
+        return word + 'ing' #most general case. If word doesn't match any of 
+                            #the aove cases return word with ing added
+print(make_ing_form('see'))
+print(make_ing_form('lie'))
+print(make_ing_form('move'))
+print(make_ing_form('hug')) #test cases
 
 #Problem 7: 
 from functools import reduce #imports reduce funtion since it is not in 
@@ -233,11 +267,15 @@ def max_in_list(numbers):
    
     ml2016
     """    
-
+    #reduce has two arguments and function and a variable, in our case a list
+    #for the function argument write a lambda function that finds the max number 
+    #second argument is the numbers that we have inputted
     
     result = reduce(lambda x,y: x if x > y else y, numbers )
     
     return result
+
+print(max_in_list([1, 50, 150, 27]))
     
     
 #Problem 8: Write a function that blanks in three ways
@@ -247,41 +285,64 @@ def max_in_list(numbers):
 
 def listlength(word):
     """
-    The function listlength(word) takes a blank and does blank using a 
-    for loop.
+    The function listlength(word) takes a list of words and maps it to
+    a list of integers representing the lengths of the corresponding 
+    words using a for loop.
     Parameters:
     
     Results:
     
     ml2016
     """
-    result = []
-    for x in word:
-        l = len(x)
-        result.append(l)
+    result = [] #sets result as an empty list
+    for x in word: 
+        #loops through our blank and  
+        l = len(x) #new variable l is the length of variable x
+        result.append(l) #result.append adds l to our result list
     return result
 
-print(listlength(['The', 'class', 'is', 'teaching', 'python']))
+print(listlength(['The', 'class', 'teaches', 'us', 'python'])) #test case
 
 def listlength(word):
+    """
+    The function listlength(word) takes a list of words and maps it to
+    a list of integers representing the lengths of the corresponding 
+    words using the map function
+    Parameters:
+    
+    Results:
+    
+    ml2016
+    """
+    #use the map function to apply the length function to each word in the 
+    #parameters
+    #return a result as a list of these lengths
     return list(map(len,word))
 
-print(listlength(['The', 'class', 'is', 'teaching', 'python']))    
+print(listlength(['The', 'class', 'teaches', 'us', 'python'])) #test case   
     
 def listlength(word):
-    length = [len(i) for i in word]
+    """
+    The function listlength() takes a blank and does blank using 
+    list comprehensions
+    
+    Parameters:
+    
+    Results:
+    
+    ml2016
+    """
+    #Use list comprehensions to apply the length function len() to each
+    #list comprehension uses square brackets and a for statement inside(?)
+    #x, or word, in our list of words
+    length = [len(x) for x in word]
     return length
 
-print(listlength(['The', 'class', 'is', 'teaching', 'python']))
+print(listlength(['The', 'class', 'teaches', 'us', 'python'])) #test case
 
     
 #Problem 9: 
-#this is three functions in one, first is the max function which blank
-#then is the map function which takes a function and applies to each blank
-#third is the len function which yields the length of our input, in
-#this case our list of words
-#so the map function will apply the length function to each of the words
-#then the max function will take the largest of the lengths and display it
+
 
 def find_longest_word(words):
     """
@@ -293,7 +354,12 @@ def find_longest_word(words):
     
     ml2016
     """
-    return max(map((len, words)))
+    #First use the length function to find the length of each word
+    #then use map to apply len() to each word in our list
+    #finally use max function to find biggest length 
+   
+    return max(map(len, words))
+print(find_longest_word(['the', 'runner','came','in', 'eleventh']))
     
 #Problem 10: 
 def filter_long_words(wordlist, n):
@@ -307,14 +373,16 @@ def filter_long_words(wordlist, n):
     
     ml2016
     """
-    return(list(filter(lambda x: len(x) > n, wordlist)))
+    #filter function does blank
+    #filter has two arguments a function and a blank
+    
+    return list(filter(lambda x: len(x) > n, wordlist))
     
 
 #Problem 11: Solve problem 1 again except this time use the map function
- #This time, as speicified in the problem we use the map function
+#This time, as speicified in the problem we use the map function
  
- #In this case it is our lambda function which takes something and spits out the 
- #swedish
+
 def translatemap(words):
     """
     The function translatemap uses the higher order function map to take a list
@@ -330,13 +398,15 @@ def translatemap(words):
     ml2016
     """
     dict2={"merry": "god", "christmas": "jul", "and": "och", "happy": "gott", 
-           "new": "nytt", "year": "ar"} #input the dictionary of english and swedish words
+           "new": "nytt", "year": "ar"} #input the dictionary of english and 
+                                        #swedish words
 
     result = map(lambda x: dict2[x], words) #applies the lambda function to
                                             #the words put into the function
    
     return list(result) #return result, we have to return it using list 
                         #because we want blank
+print(translatemap('merry christmas and happy new year'))
 
 
 #Problem 12: Implement the higher order functions map, filter and reduce
@@ -351,6 +421,9 @@ def ourmap(func, word):
     
     ml2016
     """
+    #since map applies a function to every element in a list we want to use a 
+    #for loop to implement map
+    #we will apply our function func() to each x in our second parameter, word
     
     result = []
     for x in word:
